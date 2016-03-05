5414682194306baf51d9e03369a058bdbd6de63a
willahine <willa.hine@gmail.com>
2016-03-05 10:17:07 -0500

    I kind of have week 05 homework done...
    Going to submit later

---
+++ class06/10.5_Reviews/cash_register/starter_code/js/index.js
@@ -0,0 +1,36 @@
+// total will store the running total, starting at 0
+
+
+// `enter()` reads the user's input, and updates the receipt
+
+    // Set up variables to use
+
+    // Prevent form submission
+
+    // Read the user input
+
+    // Convert to decimal
+
+    // Convert to currency
+
+    // Add line item to table
+
+    // Update the running total
+
+    // Print the updated total with proper currency formatting
+
+    // Clear out the input field, ready for next value
+
+
+// Use this to convert a number to U.S. currency format
+
+    // Convert the number to decimal
+
+    // Round to 2 decimal places
+
+    // Add the $ before the number
+
+    // Send back the result
+
+
+// When the user submits the form, run the `enter` function
